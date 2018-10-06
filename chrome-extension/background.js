dcf14e6cf304239d78cd301072291578b85fedb8
Christopher Swenor <cswenor@gmail.com>
2018-10-06 13:34:20 -0700

    Made all the needed fixes

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,14 +1,9 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-          conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
-          })
-          ],
-              actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-      });
-  });
+// this is the background code...
+
+// listen for our browerAction to be clicked
+chrome.tabs.onUpdated.addListener(function () {
+  // for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript({
+		file: 'inject.js'
+	});
+});
